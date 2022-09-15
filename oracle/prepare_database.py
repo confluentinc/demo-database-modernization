9fa2f1d18f03c9a3426bf7383742f67ba6203b8a
chuck-confluent <chuck@confluent.io>
2022-09-15 12:38:31 -0700

    organize oracle files

--- oracle/prepare_database.py
+++ oracle/prepare_database.py
@@ -1,5 +1,6 @@
 import oracledb
 import oracle_config
+from pathlib import Path
 
 def execute_file(connection: oracledb.Connection, sql_file: str) -> None:
     cursor = connection.cursor()
@@ -23,6 +24,8 @@ def execute_file(connection: oracledb.Connection, sql_file: str) -> None:
 
 if __name__=="__main__":
 
+    sql_populate_database = Path(__file__).absolute().with_name("populate_database.sql")
+    sql_enable_cdc = Path(__file__).absolute().with_name("enable_cdc.sql")
     with oracledb.connect(
         user=oracle_config.username,
         password=oracle_config.password,
@@ -30,7 +33,7 @@ if __name__=="__main__":
         port=oracle_config.port) as connection:
 
         print("Populating CUSTOMERS table")
-        execute_file(connection, "populate_database.sql")
+        execute_file(connection, sql_populate_database)
 
         print("\nEnabling Change Data Capture on CUSTOMERS table")
-        execute_file(connection, "enable_cdc.sql")
+        execute_file(connection, sql_enable_cdc)
